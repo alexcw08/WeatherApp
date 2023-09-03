733a6fbe502bcffa8145d6fcb4ba01bafdf38001
Alex C <calex781@gmail.com>
2023-09-03 12:54:02 -0400

    Add search bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import CityCard from "./components/cityCard";
+import SearchBar from "./components/searchBar";
 
 function App() {
   return (
-    <div className="bg-violet-200 flex flex-col justify-center items-center p-5 ">
+    <div className="bg-violet-200 flex flex-col p-5 items-center ">
+      <SearchBar />
       <CityCard />
     </div>
   );
---
+++ src/components/searchBar.tsx
@@ -0,0 +1,31 @@
+function searchBar() {
+  return (
+    <div className="flex rounded-lg w-2/3 md:w-1/3 bg-violet-400 m-3">
+      <input
+        className="w-full bg-violet-400 placeholder-slate-200 rounded-xl text-center text-slate-100"
+        type="number"
+        name=""
+        id=""
+        placeholder="Search a Zip Code"
+      />
+      <div className="w-10 cursor-pointer mx-2">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={1.5}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default searchBar;
