54061f99cbc43be040191d5380f5657a17bd1888
Alex C <calex781@gmail.com>
2023-09-19 18:28:43 -0400

    Render hourly forecast using api data.

--- src/components/hourlyCard.tsx
+++ src/components/hourlyCard.tsx
@@ -1,20 +1,21 @@
 import HourContainer from "./hourContainer";
 
-function hourlyCard() {
-  const data: string[][] = [
-    ["8:00am", "67", "Cloudy"],
-    ["9:00am", "69", "Cloudy"],
-    ["10:00am", "70", "Sunny"],
-    ["11:00am", "65", "Cloudy"],
-    ["12:00pm", "62", "Cloudy"],
-    ["01:00am", "64", "Sunny"],
-    ["02:00pm", "62", "Cloudy"],
-    ["03:00pm", "70", "Sunny"],
-    ["04:00pm", "65", "Cloudy"],
-    ["05:00pm", "62", "Cloudy"],
-    ["06:00am", "64", "Sunny"],
-    ["07:00pm", "62", "Cloudy"],
-  ];
+type HourlyProps = {
+  hourlyData: hourObj[];
+};
+interface hourObj {
+  time?: string;
+  temp?: number;
+  description?: string;
+}
+
+function hourlyCard(props: HourlyProps) {
+  props.hourlyData.map((time) => {
+    console.log(time.time);
+    console.log(time.temp);
+    console.log(time.description);
+  });
+
   return (
     <div className="flex flex-col bg-violet-700 text-slate-100 rounded-xl w-11/12 mb-7">
       <div className="flex mt-6 ml-10 mr-8 text-xl text-violet-100 justify-between mb-3">
@@ -22,11 +23,11 @@ function hourlyCard() {
         <h1 className="text-violet-400 text-lg">Next 12 Hours</h1>
       </div>
       <div className="flex flex-col w-11/12 mx-7 md:flex-row md:justify-evenly md:flex-wrap mb-4 ">
-        {data.map((hour) => (
+        {props.hourlyData.map((hour) => (
           <HourContainer
-            hour={hour[0]}
-            temperature={hour[1]}
-            description={hour[2]}
+            hour={hour.time}
+            temperature={hour.temp}
+            description={hour.description}
           />
         ))}
       </div>
